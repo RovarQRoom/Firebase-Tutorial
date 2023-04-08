9b20b0e700861b282cc89bf5c883c49504098386
RovarQRoom <rovar.07000120@outlook.com>
2023-04-08 09:38:41 +0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app"; // Import the initializeApp function
+
+const firebaseConfig = { // Your web app's Firebase configuration
+    apiKey: "AIzaSyC8Zo2a-l-IsyaacNwma3ks8A2KRvVRUME",
+    authDomain: "fir-tutorial-30691.firebaseapp.com",
+    projectId: "fir-tutorial-30691",
+    storageBucket: "fir-tutorial-30691.appspot.com",
+    messagingSenderId: "140222711415",
+    appId: "1:140222711415:web:7150c56c8b1fab6657e872"
+  };
+
+initializeApp(firebaseConfig); // Initialize Firebase The First Step
